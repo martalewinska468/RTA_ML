9f1d01b3296ef23e72b63e2c796ad14d341ed5f5
Marta Lewinska <martalewa468@gmail.com>
2026-04-12 09:43:13 +0000

    Lab 1

--- consumer_anomaly.py
+++ consumer_anomaly.py
@@ -1,19 +1,18 @@
 from kafka import KafkaConsumer
 import json
 import time
-from collections import defaultdict
 
-# Konfiguracja - używamy 'broker:9092' tak jak w działającym kodzie stats
+
 consumer = KafkaConsumer(
     'transactions',
     bootstrap_servers='broker:9092',
-    group_id='group_anomaly',
+    group_id='group_anomaly_clean',
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
     auto_offset_reset='latest'
 )
 
-# Słownik do przechowywania czasów transakcji użytkowników
-user_history = defaultdict(list)
+
+user_history = {}
 
 print("Monitoring anomalii prędkości (3+ transakcje w 60s)...")
 
@@ -22,13 +21,17 @@ for message in consumer:
     user_id = event['user_id']
     current_time = time.time()
     
-    # 1. Dodaj czas obecnej transakcji do historii
+    
+    if user_id not in user_history:
+        user_history[user_id] = []
+    
+    
     user_history[user_id].append(current_time)
     
-    # 2. Usuń czasy starsze niż 60 sekund
+    
     user_history[user_id] = [t for t in user_history[user_id] if current_time - t <= 60]
     
-    # 3. Sprawdź warunek (więcej niż 3 transakcje)
+    
     if len(user_history[user_id]) > 3:
         print(f"!!! ALERT: Anomalia dla użytkownika {user_id} !!!")
         print(f"Liczba transakcji w 60s: {len(user_history[user_id])}")
